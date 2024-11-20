4d6c365a984bba8d7a924e8ef645ab383a9a5db8
b-u-g-g <tsakshi1708@gmail.com>
2024-11-20 22:32:24 +0530

    page 3

--- lib/views/ui/onboarding/widgets/page_three.dart
+++ lib/views/ui/onboarding/widgets/page_three.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:jobhub_v1/views/common/custom_outline_btn.dart';
 import 'package:jobhub_v1/views/common/exports.dart';
@@ -18,67 +19,65 @@ class PageThree extends StatelessWidget {
         width: width,
         height: hieght,
         color: const Color(0xFF040326),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+        child: Stack(
           children: [
-            const HeightSpacer(size: 120),
-            Image.asset(
-              'assets/images/grouped.png',
-              height: 500,
-            ),
-            const HeightSpacer(size: 1),
+            // Existing layout (background image placement remains unchanged)
             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                CustomOutlineBtn(
-                  onTap: () async {
-                    final prefs = await SharedPreferences.getInstance();
-                    await prefs.setBool('entrypoint', true);
-                    await Get.to(
-                      () => const LoginPage(
-                        drawer: true,
-                      ),
-                    );
-                  },
-                  text: 'Login',
-                  width: width * 0.4,
-                  hieght: hieght * 0.06,
-                  color: Color(kLight.value),
+                const HeightSpacer(size: 120),
+                Image.asset(
+                  'assets/images/grouped.png',
+                  height: 500,
                 ),
-                const HeightSpacer(size: 10),
-                GestureDetector(
-                  onTap: () {
-                    Get.to(() => const RegistrationPage());
-                  },
-                  child: Container(
+              ],
+            ),
+            // Overlay the buttons on top of the image
+            Positioned(
+              top:
+                  340, // Adjust the top value to position the buttons over the image
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  CustomOutlineBtn(
+                    onTap: () async {
+                      final prefs = await SharedPreferences.getInstance();
+                      await prefs.setBool('entrypoint', true);
+                      await Get.to(
+                        () => const LoginPage(
+                          drawer: true,
+                        ),
+                      );
+                    },
+                    text: 'Login',
                     width: width * 0.4,
-                    height: hieght * 0.06,
+                    hieght: hieght * 0.06,
                     color: Color(kLight.value),
-                    child: Center(
-                      child: ReusableText(
-                        text: 'Sign Up',
-                        style: appstyle(
-                          16,
-                          Color(kLightBlue.value),
-                          FontWeight.w600,
+                  ),
+                  const SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () {
+                      Get.to(() => const RegistrationPage());
+                    },
+                    child: Container(
+                      width: width * 0.4,
+                      height: hieght * 0.06,
+                      color: Color(kLight.value),
+                      child: Center(
+                        child: ReusableText(
+                          text: 'Sign Up',
+                          style: appstyle(
+                            16,
+                            Color(kLightBlue.value),
+                            FontWeight.w600,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                const HeightSpacer(size: 20),
-                GestureDetector(
-                  onTap: () async {
-                    final prefs = await SharedPreferences.getInstance();
-                    await prefs.setBool('entrypoint', true);
-                    await Get.to(() => const MainScreen());
-                  },
-                  child: ReusableText(
-                    text: 'Continue as guest',
-                    style: appstyle(16, Color(kLight.value), FontWeight.w400),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
