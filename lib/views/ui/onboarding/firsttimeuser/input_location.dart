88038cfcf0f7ad242413d16dda881ee1300da2c3
b-u-g-g <tsakshi1708@gmail.com>
2025-03-16 01:05:14 +0530

    input location

--- lib/views/ui/onboarding/firsttimeuser/input_location.dart
+++ lib/views/ui/onboarding/firsttimeuser/input_location.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'homepage.dart'; // Import the HomePage
+import 'package:jobhub_v1/views/ui/homepage.dart';
+// Import the HomePage
 
 class Page4 extends StatefulWidget {
   @override
@@ -12,16 +14,9 @@ class _Page4State extends State<Page4> {
   String? selectedState;
   String? selectedCountry;
 
-  final List<String> states = [
-    'State 1',
-    'State 2',
-    'State 3'
-  ]; // Example states
-  final List<String> countries = [
-    'Country 1',
-    'Country 2',
-    'Country 3'
-  ]; // Example countries
+  // Example lists for dropdowns
+  final List<String> states = ['State 1', 'State 2', 'State 3'];
+  final List<String> countries = ['Country 1', 'Country 2', 'Country 3'];
 
   @override
   void dispose() {
@@ -32,81 +27,169 @@ class _Page4State extends State<Page4> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Enter Location Details'),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: cityController,
-              decoration: const InputDecoration(
-                labelText: 'City',
-                hintText: 'Enter your city',
-                border: OutlineInputBorder(),
-              ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF8E2DE2), Color(0xFF4A00E0)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 30),
+                Center(
+                  child: Text(
+                    'Enter Location Details',
+                    style: TextStyle(
+                      fontSize: 30.sp,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Text(
+                  'City',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: cityController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: 'Enter your city',
+                    hintStyle: TextStyle(color: Colors.white60),
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.2),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'State',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                DropdownButtonFormField<String>(
+                  value: selectedState,
+                  items: states.map((state) {
+                    return DropdownMenuItem<String>(
+                      value: state,
+                      child: Text(state),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedState = value;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.2),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    hintText: 'Select your state',
+                    hintStyle: TextStyle(color: Colors.white60),
+                  ),
+                  dropdownColor: Colors.deepPurple.shade700,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Country',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                DropdownButtonFormField<String>(
+                  value: selectedCountry,
+                  items: countries.map((country) {
+                    return DropdownMenuItem<String>(
+                      value: country,
+                      child: Text(country),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedCountry = value;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.2),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    hintText: 'Select your country',
+                    hintStyle: TextStyle(color: Colors.white60),
+                  ),
+                  dropdownColor: Colors.deepPurple.shade700,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 40),
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          const Color(0xFF4A00E0), // Button background
+                      padding: EdgeInsets.symmetric(
+                        vertical: 15.h,
+                        horizontal: 50.w,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (cityController.text.isNotEmpty &&
+                          selectedState != null &&
+                          selectedCountry != null) {
+                        // Navigate to HomePage
+                        Get.offAll(() => HomePage());
+                      } else {
+                        Get.snackbar(
+                          'Error',
+                          'Please fill all fields',
+                          colorText: Colors.white,
+                          backgroundColor: Colors.red,
+                        );
+                      }
+                    },
+                    child: Text(
+                      'Submit',
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            DropdownButtonFormField<String>(
-              value: selectedState,
-              items: states.map((state) {
-                return DropdownMenuItem<String>(
-                  value: state,
-                  child: Text(state),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  selectedState = value;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'State',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            DropdownButtonFormField<String>(
-              value: selectedCountry,
-              items: countries.map((country) {
-                return DropdownMenuItem<String>(
-                  value: country,
-                  child: Text(country),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  selectedCountry = value;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'Country',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                if (cityController.text.isNotEmpty &&
-                    selectedState != null &&
-                    selectedCountry != null) {
-                  // Navigate to HomePage
-                  Get.offAll(() => HomePage());
-                } else {
-                  Get.snackbar(
-                    'Error',
-                    'Please fill all fields',
-                    backgroundColor: Colors.red,
-                    colorText: Colors.white,
-                  );
-                }
-              },
-              child: const Text('Submit'),
-            ),
-          ],
+          ),
         ),
       ),
     );
